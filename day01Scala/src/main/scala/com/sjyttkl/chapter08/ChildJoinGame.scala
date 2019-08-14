7b1d3a94287e41966ca6281e8b7f00a2b158595b
songdongdong2 <songdongdong@jd.com>
2019-08-14 21:23:23 +0800

    学习 伴生对象

---
+++ day01Scala/src/main/scala/com/sjyttkl/chapter08/ChildJoinGame.scala
@@ -0,0 +1,40 @@
+package com.sjyttkl.chapter08
+
+/**
+  * Create with: com.sjyttkl.chapter08 
+  * author: songdongdong  
+  * E-mail: songdongdong@jd.com 
+  * date: 2019/8/14 21:15 
+  * version: 1.0
+  * description:  
+  */
+
+object ChildJoinGame {
+  def main(args: Array[String]): Unit = {
+    //创建三个小孩
+    val child0 = new Child02("白骨精")
+    val child1 = new Child02("蜘蛛精")
+    val child2 = new Child02("黄鼠狼精")
+    Child02.joinGame(child0)
+    Child02.joinGame(child1)
+    Child02.joinGame(child2)
+    Child02.showNum()
+  }
+}
+
+class Child02(cName: String) {
+  var name = cName
+}
+
+object Child02 {
+  //统计共有多少小孩的属性
+  var totalChildNum = 0
+
+  def joinGame(child: Child02): Unit = {
+    printf("%s 小孩加入了游戏\n", child.name)
+    //totalChildNum 加1
+    totalChildNum += 1
+  }
+
+  def showNum(): Unit = {
+    printf("当前有%d小孩玩游戏\n", totalChildNum)
---
+++ day01Scala/src/main/scala/com/sjyttkl/chapter08/TestChildGame.scala
@@ -0,0 +1,27 @@
+package com.sjyttkl.chapter08
+
+/**
+  * Create with: com.sjyttkl.chapter08 
+  * author: sjyttkl
+  * E-mail: 6965492835@qq.com
+  * date: 2019/8/14 20:50 
+  * version: 1.0
+  * description:  
+  */
+object TestChildGame {
+  def main(args: Array[String]): Unit = {
+    /**
+      *
+      *
+      */
+    //传统方法...不好，怎么办?=>静态概念
+    //创建很多小孩，加入游戏
+    //定义一个变量 totalNum ...
+    val totalNum = 0
+
+  }
+}
+
+class child {
+
+}
