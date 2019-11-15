711eb6eeab25a5ce4f7df0489e434597e5663a58
songdongdong2 <songdongdong@jd.com>
2019-11-15 18:01:47 +0800

    混入学习下 trait

---
+++ src/main/scala/com/sjyttkl/chapter08/mixin/example.scala
@@ -0,0 +1,18 @@
+package com.sjyttkl.chapter08.mixin
+
+import com.sjyttkl.chapter08.mixin.MixInDemo02.DB5
+
+/**
+  * Create with: com.sjyttkl.chapter08.mixin 
+  * author: songdongdong  
+  * E-mail: songdongdong@jd.com 
+  * date: 2019/11/10 11:44 
+  * version: 1.0
+  * description:  
+  */
+object example {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
+
