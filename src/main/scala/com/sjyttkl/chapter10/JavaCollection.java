eef331779df821414f5714b4ab5e647b3fcab38f
songdongdong2 <songdongdong@jd.com>
2019-11-21 12:50:12 +0800

    集合

---
+++ src/main/scala/com/sjyttkl/chapter10/JavaCollection.java
@@ -0,0 +1,32 @@
+package com.sjyttkl.chapter10;
+
+import java.util.ArrayList;
+
+/**
+ * Create with: com.sjyttkl.chapter09
+ * author: sjyttkl
+ * E-mail: 695492835@qq.com
+ * date: 2019/11/21 12:46
+ * version: 1.0
+ * description: 集合
+ */
+public class JavaCollection {
+    public static void main(String[] args){
+        //不可变集合类似java的数组
+        int[] nums = new int[3];
+        nums[2] = 11; //?
+        nums[2] = 22;
+        //nums[3] = 90; //?
+
+//        String[] names = {"bj", "sh"};
+//        System.out.println(nums + " " + names);
+//
+//        //可变集合举例
+        ArrayList al = new ArrayList<String>();
+        al.add("zs");
+        al.add("zs2");
+        System.out.println(al + " 地址= " + al.hashCode()); //地址
+        al.add("zs3");
+        System.out.println(al + " 地址2=" + al.hashCode()); //地址
+    }
+}
